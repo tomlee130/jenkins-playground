fd516b87ceea9e78bb23c410aec7b7b9925f3d01
Esa Firman <esafirm21@gmail.com>
2019-10-08 15:48:42 +0700

    initial commit

---
+++ TheSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheSample
+//
+//  Created by Esa Firman on 08/10/19.
+//  Copyright © 2019 Esa Firman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
